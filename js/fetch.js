47352904cdba36d6429f3913cea73672754fa604
luoqiuyun <qiuyun.luo@outlook.com>
2017-06-15 17:11:19 -0400

    updated

--- js/fetch.js
+++ js/fetch.js
@@ -1,8 +1,24 @@
+var likes = {
+  "likes": {"screenName": 'bess_luo'},
+  "displayTitle": 'Likes by Qiu Yun',
+  "domId": 'LikesQiuYun',
+  "numId": 'LikesLaughingsquidNum',
+  "maxTweets": 60,
+  "enableLinks": true, 
+  "showUser": true,
+  "showTime": true,
+  "showImages": true,
+  "linksInNewWindow": true,
+  "lang": 'en'
+};
+twitterFetcher.fetch(likes);
+
 var configList = {
   "list": {"listSlug": 'worked', "screenName": 'bess_luo'},
+  "displayTitle": 'List Qiu Yun',
   "domId": 'ListQiuYun',
-  "numId": 'ListQiuYunNum',//qiu
-  "maxTweets": 5,
+  "numId": 'ListQiuYunNum',
+  "maxTweets": 60,
   "enableLinks": true, 
   "showUser": true,
   "showTime": true,
@@ -11,12 +27,12 @@ var configList = {
 };
 twitterFetcher.fetch(configList);
 
-
 var configProfile = {
   "profile": {"screenName": 'bess_luo'},
+  "displayTitle": 'Profile Qiu Yun',
   "domId": 'ProfileQiuYun',
-  "numId": 'ProfileQiuYunNum',//qiu
-  "maxTweets": 3,
+  "numId": 'ProfileQiuYunNum',
+  "maxTweets": 60,
   "enableLinks": true, 
   "showUser": true,
   "showTime": true,
@@ -27,6 +43,7 @@ twitterFetcher.fetch(configProfile);
 
 var profile = {
   "profile": {"screenName": 'laughingsquid'},
+  "displayTitle": 'Profile laughingsquid',
   "domId": 'ProfileLaughingsquid',
   "numId": 'ProfileLaughingsquidNum',
   "maxTweets": 60,
@@ -41,6 +58,7 @@ twitterFetcher.fetch(profile);
 
 var likes = {
   "likes": {"screenName": 'laughingsquid'},
+  "displayTitle": 'Likes by laughingsquid',
   "domId": 'LikesLaughingsquid',
   "numId": 'LikesLaughingsquidNum',
   "maxTweets": 60,
@@ -55,6 +73,7 @@ twitterFetcher.fetch(likes);
 
 var profile = {
   "profile": {"screenName": 'techcrunch'},
+  "displayTitle": 'Profile techcrunch',
   "domId": 'ProfileTechcrunch',
   "numId": 'ProfileTechcrunchNum',
   "maxTweets": 60,
@@ -69,6 +88,7 @@ twitterFetcher.fetch(profile);
 
 var likes = {
   "likes": {"screenName": 'techcrunch'},
+  "displayTitle": 'Likes by techcrunch',
   "domId": 'LikesTechcrunch',
   "numId": 'LikesTechcrunchNum',
   "maxTweets": 60,
@@ -83,6 +103,7 @@ twitterFetcher.fetch(likes);
 
 var likes = {
   "likes": {"screenName": 'AppDirect'},
+  "displayTitle": 'Likes by AppDirect',
   "domId": 'LikesAppDirect',
   "numId": 'LikesAppDirectNum',
   "maxTweets": 60,
@@ -97,6 +118,7 @@ twitterFetcher.fetch(likes);
 
 var profile = {
   "profile": {"screenName": 'AppDirect'},
+  "displayTitle": 'Profile AppDirect',
   "domId": 'ProfileAppDirect',
   "numId": 'ProfileAppDirectNum',
   "maxTweets": 60,
